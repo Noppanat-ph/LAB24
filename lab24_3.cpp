26731c97dbdcd19dc8b2e31cd1e9e6e589351dc2
Noppanat <pangpondzaza.1604@gmail.com>
2026-03-09 17:55:59 +0700

    Finish

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
 //Write function count() here.
+int count(int d[],int n){
+    int count = 0;
+    set<int> data;
+    for(int i = 0; i < n; i++){
+        data.insert(d[i]);
+    }
+    set<int>::iterator i;
+    for(i = data.begin(); i != data.end(); i++){
+        count++;
+    }
+    return count;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
